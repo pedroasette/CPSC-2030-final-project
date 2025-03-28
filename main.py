06f366a756e7fe0321599d1f6081759326f94240
pedro <pedro.sette231@gmail.com>
2025-03-27 20:21:41 -0400

    Upper case evaluator

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 class Password(ABC):
     def __init__(self, password):
-        self.password = password
+        self.password = str(password)
         self.evaluator = 0
  
     @abstractmethod
@@ -12,17 +12,30 @@ class Password(ABC):
 class PasswordLenght(Password):
     def __init__(self, password):
         super().__init__(password)
+
     def evaluate(self):
-        if len(str(self.password)) < 8:
+        if len(self.password) < 8:
             self.evaluator +=1
-        elif 8 <= len(str(self.password)) < 12:
+        elif 8 <= len(self.password) < 12:
             self.evaluator +=2
-        elif 12 < len(str(self.password)):
+        elif 12 < len(self.password):
             self.evaluator +=3
+
+class PasswordUpperCase(Password):
+    def __init__(self, password):
+        super().__init__(password)
+
+    def evaluate(self):
+        for i in self.password:
+            if i.isupper():
+                self.evaluator +=3
+                break
+
+
             
 
 
-test = PasswordLenght(12345678912345)
+test = PasswordUpperCase("C1234")
 
 test.evaluate()
 
